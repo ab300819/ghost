b40c1cd68b452c796771cdc39e8feeac75aabc1b
Mason <ab300819@163.com>
2020-09-29 01:53:08 +0800

    feat: add qt opengl file

---
+++ src/widget/paint_widget.cpp
@@ -0,0 +1,5 @@
+//
+// Created by 孟伸 on 2020/9/29.
+//
+
+#include "paint_widget.h"
---
+++ src/widget/paint_widget.h
@@ -0,0 +1,15 @@
+//
+// Created by 孟伸 on 2020/9/29.
+//
+
+#pragma once
+#include <QOpenGLBuffer>
+#include <QOpenGLContext>
+#include <QOpenGLFunctions>
+#include <QOpenGLShaderProgram>
+#include <QOpenGLVertexArrayObject>
+#include <QOpenGLWidget>
+
+class PaintWidget : public QOpenGLWidget {
+
+};
