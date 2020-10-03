9b52f51ba8cbe5ba03ee6ba0b828186bfe3c6aa4
Mason <ab300819@163.com>
2020-10-04 01:04:58 +0800

    fix: set profile for macOS

--- src/widget/main_ui.cpp
+++ src/widget/main_ui.cpp
@@ -3,9 +3,15 @@
 #include <QApplication>
 
 int main(int argc, char *argv[]) {
+	
+	QSurfaceFormat glFormat;
+	glFormat.setVersion(3,3);
+	glFormat.setProfile(QSurfaceFormat::CoreProfile);
+	QSurfaceFormat::setDefaultFormat(glFormat);
+
 	QApplication a(argc, argv);
 	PaintWidget w(nullptr);
 	w.resize(800, 600);
 	w.show();
-	return a.exec();
+	return QApplication::exec();
 }
